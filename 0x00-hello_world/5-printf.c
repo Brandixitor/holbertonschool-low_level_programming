937f20a52f4cbcc65e25526321614e820f1e84c1
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-16 17:34:07 +0100

    " "

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/**
- *Printf?
- *I love Betty
- *C_is_fun
- */
+/*
+*Printf?
+*I love Betty
+*C_is_fun
+*/
 int main (void)
 {
-  printf ("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf ("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
