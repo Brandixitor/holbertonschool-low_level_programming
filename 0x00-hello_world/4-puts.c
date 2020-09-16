dcfd397fe3f60702b343530f331d136c779d78bc
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-16 17:55:21 +0100

    " "

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
-*This function prints a message and returns the value 0
-*main - puts a character
-*C_is_fun
-*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
