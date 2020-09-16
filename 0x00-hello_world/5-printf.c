481509aef9de7114eb3584db65043493650827d3
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-16 17:36:18 +0100

    " "

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
 *I love Betty
 *C_is_fun
 */
-int main (void)
+int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
 }
