24e62ff2418f734a7b05c6571c72bc5682e29cb6
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-16 12:32:35 +0100

    " "

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /*
  *This function does x and y 
  *I love Betty
@@ -6,6 +6,6 @@
 */
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+          puts("\"Programming is like building a multilingual puzzle");
+          return (0);
 }
