5b44eb2329702d37a7e8bbda93eadf17adc64c46
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-16 19:38:51 +0100

    " "

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int i;
 long int li;
 long long int lli;
 float f;
-printf("Size of a char: %i byte(s)\n", sizeof(char));
-printf("Size of a char: %i byte(s)\n", sizeof(int));
-printf("Size of a char: %i byte(s)\n", sizeof(long int));
-printf("Size of a char: %i byte(s)\n", sizeof(long long int));
-printf("Size of a char: %i byte(s)\n", sizeof(float));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of a char: %lu byte(s)\n", sizeof(int));
+printf("Size of a char: %lu byte(s)\n", sizeof(long int));
+printf("Size of a char: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a char: %lu byte(s)\n", sizeof(float));
 return (0);
 }
