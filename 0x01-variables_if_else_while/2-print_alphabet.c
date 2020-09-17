2bff09b3fd9864f118a52784fdbf6116fa1a84d8
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-17 14:17:26 +0100

    " "

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- *main - main function
- *
- *Return: 0
- */
+*main - maine function
+*Return: 0
+*/
 int main(void)
 {
-char i;
-for (i = 'a'; i <= 'z'; ++i)
-{
-putchar(i);
-}
-putchar('\n')
-return (0);
+	char i;
+
+	for (i = 'a'; i <= 'z'; ++i)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
