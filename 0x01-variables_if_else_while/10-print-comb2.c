a0fca4e15d1e7402d54b914eb6ff36954b0783ae
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-17 16:50:44 +0100

    " "

--- 0x01-variables_if_else_while/10-print-comb2.c
+++ 0x01-variables_if_else_while/10-print-comb2.c
@@ -9,9 +9,9 @@ int main(void)
 int x;
 int e;
 
-for (x = '0'; x <= '9'; ++i)
+for (x = '0'; x <= '9'; ++x)
 {
-for (e = '0'; e <= '9'; ++j)
+for (e = '0'; e <= '9'; ++e)
 {
 if (x != '0' || e != '0')
 {
