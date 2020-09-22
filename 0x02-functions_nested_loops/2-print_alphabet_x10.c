8168a52ea4038a0643cac5f0a25b79b6894f066d
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-22 10:14:09 +0100

    Bug fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,20 @@
 #include "holberton.h"
 /**
-*main- main function
+* print_alphabet_x10 - check the code for Holberton School students.
 *
-*Return: 0
+* Return: no return.
 */
 void print_alphabet_x10(void)
-		{
+{
+char i;
+int j;
 
-		}
+for (j = 1; j <= 10; ++j)
+{
+for (i = 'a'; i <= 'z'; ++i)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
