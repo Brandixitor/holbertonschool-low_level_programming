c4a9069d149e37e9270ee1a7f392a1a70f8eda90
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-22 16:50:00 +0100

    Infinite loop!

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,9 +7,9 @@
 
 int main(void)
 {
-/*    int i; */
+    /*    int i; */
 
-printf("Infinite loop incoming :(\n");
+		printf("Infinite loop incoming :(\n");
 /*
 *i = 0;
 *while (i < 10)
@@ -17,7 +17,7 @@ printf("Infinite loop incoming :(\n");
 *putchar(i);
 *}
 */
-printf("Infinite loop avoided! \\o/\n");
+		printf("Infinite loop avoided! \\o/\n");
 
-return (0);
+		return (0);
 }
