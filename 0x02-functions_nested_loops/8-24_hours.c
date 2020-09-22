e16294c5278db7036d9a12c091743016144cf4c1
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-22 13:51:41 +0100

    What's the time?

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,11 +12,11 @@ for (i = 0; i < 24; ++i)
 {
 for (j = 0; j < 60; ++j)
 {
-_putchar(i / 10) + '0');
-_putchar(i % 10) + '0');
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
 _putchar(':');
-_putchar(j / 10) + '0');
-_putchar(j % 10) + '0');
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
 _putchar('\n');
 }
 }
