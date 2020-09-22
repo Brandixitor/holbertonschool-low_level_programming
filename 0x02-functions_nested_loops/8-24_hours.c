f764aa7e69f84c739dc77ce1623b4dae245e42bf
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-22 13:43:08 +0100

    What's the time?

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,15 @@ void jack_bauer(void)
 {
 int i, j;
 
-for (i = 0; i <= 23; ++i)
+for (i = 0; i < 24; ++i)
 {
-for (j = 0; j <= 59; ++j)
+for (j = 0; j < 60; ++j)
 {
-_putchar((i / 1)0 + '0');
-_putchar((i % 10) + '0');
+_putchar(i / 10) + '0');
+_putchar(i % 10) + '0');
 _putchar(':');
-_putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
+_putchar(j / 10) + '0');
+_putchar(j % 10) + '0');
 _putchar('\n');
 }
 }
