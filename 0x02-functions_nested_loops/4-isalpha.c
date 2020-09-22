d82850d2ec9a12f290a4b6af966ff49755d24b16
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-22 11:13:04 +0100

    4th task fixed

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,16 +6,13 @@
 */
 int _isalpha(int c)
 {
-if (c >= 'a' && c <= 'z')
-{
-if (c >= 'A' && c <= 'Z')
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return (1);
 }
 else
 {
 return (0);
-}
 }
 }
 
