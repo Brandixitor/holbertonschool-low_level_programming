fc92d480499c6aa4d875cdc1ba7776d2a4f6c5c9
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-22 11:36:18 +0100

    2nd fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,21 @@
 #include "holberton.h"
 /**
-*print_sign - main function
+*print_sign - 0
 *@n: integer
 *Return: either 0, 1 or -1
 */
-int print_sign(int n);
+int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar("+");
+_putchar('+');
 return (1);
 }
 if (n < 0)
 {
-_putchar("-");
+_putchar('-');
 return (-1);
 }
-_putchar("0");
+_putchar('0');
 return (0);
-}
 }
