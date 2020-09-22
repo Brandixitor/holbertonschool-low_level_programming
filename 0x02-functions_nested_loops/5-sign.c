d261421a8b3c487e6cf24d26a1658c48f2c03a81
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-22 11:24:57 +0100

    5th task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+*print_sign - main function
+*@n: integer
+*Return: either 0, 1 or -1
+*/
+int print_sign(int n);
+{
+int n;
+
+while (n > 0)
+{
+_putchar("+");
+return (1);
+}
+while (n = 0)
+{
+_putchar("0");
+return (0);
+}
+while (n < 0)
+{
+_putchar("-");
+return (-1);
+}
+}
