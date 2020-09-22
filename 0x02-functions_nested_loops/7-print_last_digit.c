99a4ce633b662258d5664a62ae46eb816172abb5
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-22 13:10:03 +0100

    Fixed!

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
-*print_last_digit - main function
+*print_last_digit - 0
 *@n: integer
-*
+*Return: last digit
 */
 int print_last_digit(int n);
 {
@@ -16,7 +16,7 @@ return (i);
 }
 else
 {
-i= n % 10;
+i = n % 10;
 _putchar(i + '0');
 return (i);
 }
