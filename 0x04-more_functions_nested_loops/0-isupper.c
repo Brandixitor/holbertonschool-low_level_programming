8d24e2be59492c7bd0e455728ee9367014078ce6
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-24 11:55:28 +0100

    Uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* _isupper - checks for uppercase characters.
+* @c: char
+* Return: either 0 or 1.
+*/
+int _isupper(int c)
+{
+if (c <= 'A' && c >= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
