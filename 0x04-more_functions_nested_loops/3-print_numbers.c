e64b17e539a6f0c1942d2632e7f4a65b6d15c37f
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-24 13:05:08 +0100

    Print all numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* print_numbers - prints numbers from 0 to 9.
+*
+* Return: output.
+*/
+void print_numbers(void)
+{
+int n;
+
+for ((n => '0') && (n <= '9'))
+{
+_putchar("%i", n);
+}
+}
