30eb965b6a59ca9260cc28fd3631611c69b08472
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-24 13:55:09 +0100

    Prints numbers!

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* print_numbers - prints numbers from 0 to 9 excluding 2 and 4.
+*
+* Return: output.
+*/
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 0; i < 10; ++i)
+{
+if (i != 2 && i != 4)
+{
+_putchar(i + '0');
+}
+}
+_putchar('\n');
+}
