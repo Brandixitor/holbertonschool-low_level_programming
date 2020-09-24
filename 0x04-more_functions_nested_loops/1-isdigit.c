70b61849b5e3ac561529982d1e4d877040cc8ef1
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-24 12:15:42 +0100

    Isgidit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* _isdigit - Checks for a digit ( 0 through 9 ).
+* @c: integer.
+* Return: either 0 or 1.
+*/
+int _isdigit(int c)
+	{
+for (c >= 0 && c <= 9)
+{
+return (1);
+}
+
+		else
+		{
+		return (0);
+		}
+	}
