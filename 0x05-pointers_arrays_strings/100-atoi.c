e426f080585e45a32c0f7f5d88c4fde98bab256a
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-29 23:20:12 +0100

    Fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,74 +5,25 @@
 * @num: Current number.
 * Return: Integer.
 */
-int incase(char *s, int num)
-{
-	switch (*s)
-	{
-	case '0':
-	num = num * 10 + 0;
-	break;
-	case '1':
-	num = num * 10 + 1;
-	break;
-	case '2':
-	num = num * 10 + 2;
-	break;
-	case '3':
-	num = num * 10 + 3;
-	break;
-	case '4':
-	num = num * 10 + 4;
-	break;
-	case '5':
-	num = num * 10 + 5;
-	break;
-	case '6':
-	num = num * 10 + 6;
-	break;
-	case '7':
-	num = num * 10 + 7;
-	break;
-	case '8':
-	num = num * 10 + 8;
-	break;
-	case '9':
-	num = num * 10 + 9;
-	break;
-	}
-	return (num);
-}
-
-
-
-
-/**
-* _atoi - converts a string to an integer.
-* @s: the string.
-* Return: the converted string to an integer.
-*/
 int _atoi(char *s)
 {
-	int num, sig;
+	int j, firstNumber = 1, negatif = 1;
+	unsigned int num = 0;
 
-	sig = 1;
-	num = 0;
-	while (*s != '\0')
+	j = 0;
+	while (s[j] && firstNumber)
 	{
-		if (*s == '-')
+		if (s[j] == '-' && !num)
+			negatif *= -1;
+		if (s[j] >= '0' && s[j] <= '9' && firstNumber)
 		{
-			sig *= -1;
+			num *= 10;
+			num += s[j] - '0';
 		}
-		while (*s >= '0' && *s <= '9')
-		{
-			num = incase(s, num);
-			s++;
-			if (!(*s >= '0' && *s <= '9'))
-			{
-				return (num * sig);
-			}
-		}
-		s++;
-	}
-	return (0);
+		else if (num > 0)
+			firstNumber = 0;
+		j++;
+	};
+	num *= negatif;
+	return (num);
 }
