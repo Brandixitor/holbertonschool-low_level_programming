680f84ebe950c4a1ed224e84d1ed99de3f21dc64
Brandixitor <med.salah.bourouba@gmail.com>
2020-09-29 11:51:11 +0100

    Betty error fixed

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,31 +6,31 @@
 */
 void puts_half(char *str)
 {
-int i, j;
+	int i, j;
 
-i = 0;
+	i = 0;
 	while (str[i] != '\0')
 	{
-	i++;
+		i++;
 	}
-		j = i;
-		i = j / 2;
+	j = i;
+	i = j / 2;
 	if (j % 2 == 0)
 	{
-			while (i < j)
-			{
-			_putchar(str[i]);
-			i++;
-			}
-	}
-else
-{
-i += 1;
 		while (i < j)
 		{
-		_putchar(str[i]);
-		i++;
+			_putchar(str[i]);
+			i++;
 		}
-}
-_putchar('\n');
+	}
+	else
+	{
+		i += 1;
+		while (i < j)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	_putchar('\n');
 }
