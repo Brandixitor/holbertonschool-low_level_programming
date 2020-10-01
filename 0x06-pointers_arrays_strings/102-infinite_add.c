6d0e303db6913617907896a2836269629f3d6991
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-01 22:33:59 +0100

    Maybe next time :(

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* infinite_add - function.
+* @n1: variable
+* @n2: variable
+* @r: variable
+* @size_r: variable
+* Return: 0
+*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+* print_buffer - function
+* @b: variable
+* @size: variable
+* Return: 0
+*/
+void print_buffer(char *b, int size)
+{
+}
