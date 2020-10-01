1eeb7e60bcbfd7d40c48c4fc6670835893807878
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-01 13:06:42 +0100

    Let's play with the TRI

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* reverse_array - function that reverses an array.
+* @a: integer.
+* @n: integer.
+* Return: a reversed array.
+*/
+
+void reverse_array(int *a, int n)
+{
+int i, aux;
+
+aux = 0;
+i = 0;
+	for (i = 0; i < n; i++);
+	{
+	aux = a[i];
+	a[i] = a[n - i - 1];
+	a[n - i - 1] = aux;
+	}
+}
