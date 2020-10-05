ee9415043549fe3025cbd99ac3d2e2d1f92e4407
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-05 13:48:48 +0100

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,14 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; i >= '\0'; i++)
+	i = 0;
+	while (s[i] >= '\0')
 	{
 		if (s[i] == c)
 		{
 		return (&s[i]);
 		}
+	i++;
 	}
 return (0);
 }
