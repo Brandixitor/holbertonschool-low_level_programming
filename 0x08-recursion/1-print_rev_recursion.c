b6d00ee8c574103f843839ebc352538c3cc997d3
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-07 09:56:09 +0100

    Let's print in revere

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+*_print_rev_recursion - function that prints a string in reverse.
+*@s: string.
+* Return: ..
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	return;
+	}
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
