449e3aa1ce68241a5f024c34681f260b093b937d
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-07 12:59:43 +0100

    SQRT

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,42 @@
 #include "holberton.h"
 /**
-* co - function that returns the natural square root of a number.
+* main - function that returns the natural square root of a number.
 * @i: integer.
 * @n: integer.
 * Return: ..
 */
 
-int co(long int i, long int n)
+int main(long int n, long int i)
 {
-	if (i * i ==  n)
-	return (i);
-	else
 	if (i * i > n)
+	{
 	return (-1);
-	else
-	if (n < 0)
-	return (-1);
-	return (co(i + 1, n));
+	}
+		if (i * i == n)
+		{
+		return (i);
+		}
+return (main(n, i + 1));
 }
 
 
 
 
-
 /**
-* _sqrt_recursion - recursive part.
+* _sqrt_recursion - function that returns the natural square root of a number.
 * @n: integer.
 * Return: ..
 */
-int _sqrt_recursion(co(1, n))
+
+int _sqrt_recursion(int n)
 {
-	return (co(1, n));
+	if (n == 0)
+	{
+	return (0);
+	}
+		if (n < 0)
+		{
+		return (-1);
+		}
+return (main(n, 1));
 }
