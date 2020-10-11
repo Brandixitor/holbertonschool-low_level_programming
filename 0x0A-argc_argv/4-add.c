16c35b3584e24ab54c1dbc92290cf6e5bfa72dcc
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-11 15:31:59 +0100

    Add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+*main - function that adds two positive integers.
+*@argc: argument count.
+*@argv: argument vector.
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+int i, j, k;
+
+i = 1;
+k = 0;
+	while (i < argc)
+	{
+	j = 0;
+		while (j < argv[i][j])
+		{
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+			{
+			printf("Error\n");
+			return (1);
+			}
+		j++;
+		}
+	r += atoi(argv[i]);
+	i++;
+	}
+printf("%d\n", k);
+return (0);
+}
