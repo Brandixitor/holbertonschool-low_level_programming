062f170a9b79526d5bf3a87315d394b17e8e9139
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-13 14:20:16 +0100

    Free as a bird

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* free_grid - function that frees a 2D grid.
+* @grid: grid address.
+* @height: height.
+* Return: ...
+*/
+void free_grid(int **grid, int height)
+{
+int i;
+
+i = 0;
+	while (i < height)
+	{
+	free (grid[i]);
+	free(grid);
+	i++;
+	}
+}
