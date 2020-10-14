095e9fd0c69f7e7b5c4e5cacd1faaacb7933f2a3
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-14 16:52:51 +0100

    Allocate

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,8 +7,12 @@
 */
 void *malloc_checked(unsigned int b)
 {
-	void *i = malloc(b);
-		if (i == NULL)
+unsigned int *i;
+
+	i = malloc(b);
+	if (i == NULL)
+	{
 		exit(98);
-	return (i);
+	}
+return (i);
 }
