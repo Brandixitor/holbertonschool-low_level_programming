a24077329ae51998a70726f416958db3feb46794
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-14 16:41:15 +0100

    Prototypes

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+#endif
