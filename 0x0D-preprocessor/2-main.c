723f6cfe75dd8450ce13be2774df7ae0f0863478
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-19 12:32:47 +0100

    Name of the executable file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* main - function that prints the name of it's executable file.
+* @void: void.
+* Return: 0.
+*/
+
+int main(void)
+{
+	_putchar("%s\n", __FILE__);
+	return (0);
+}
