b5895a7abfa8960526bf7003bc916e370fc9a2e6
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-19 14:47:18 +0100

    Prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,13 +7,16 @@
 * @owner:dog's owner.
 *
 */
+
 struct dog
-{
+	{
 		char *owner;
 		float age;
 		char *name;
-};
+	};
 typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
