dd5b9bbecb8198c146fdf2d81b7bd277c0def7f7
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-21 12:59:44 +0100

    First commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "3-calc.h"
+/**
+* op_add - sum of a and b.
+* @a: integer.
+* @b: integer.
+* Return: result.
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+
+/**
+* op_sub - difference of a and b.
+* @a: integer.
+* @b: integer.
+* Return: result.
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+
+/**
+* op_mul - product of a and b.
+* @a: integer.
+* @b: integer.
+* Return: result.
+**/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+
+/**
+* op_div - divides a from b.
+* @a: integer.
+* @b: integer.
+* Return: result.
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+
+/**
+* op_mod - mod a from b
+* @a: integer.
+* @b: integer.
+* Return: result.
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
