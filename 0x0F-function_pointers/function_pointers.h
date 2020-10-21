30b4fe54c524d00246343bddd8c3d67e9510c5ec
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-21 12:28:06 +0100

    Fixed prototypes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
+#include <stdlib.h>
 #ifndef HEADER_FILE
 #define HEADER_FILE
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
