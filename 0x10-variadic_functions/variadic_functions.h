e1e521a89c64c09726e457b34d0c84cfb60f4706
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-24 13:04:40 +0100

    Prototypes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,7 @@
 #ifndef VARIADIC_HEADER
 #define VARIADIC_HEADER
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
