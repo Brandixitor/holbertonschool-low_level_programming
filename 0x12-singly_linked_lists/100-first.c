bf6de6ab28560c09fa52d86eb18b547a5443461b
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-30 22:03:29 +0100

    First commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+* print_first - function that prints a message before the main fn  execution.
+*/
+
+void my_function(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
