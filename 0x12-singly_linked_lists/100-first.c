0505d566db2d2a2f961d5aed562d8bed9d9f67eb
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-30 22:12:15 +0100

    Fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,6 +3,8 @@
 * print_first - function that prints a message before the main fn  execution.
 */
 
+void my_function(void) __attribute__ ((constructor));
+
 void my_function(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
