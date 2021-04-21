2b9efb40f90363de0b3c26984faa2627b4d51b34
Brandixitor <med.salah.bourouba@gmail.com>
2021-04-21 15:54:10 +0100

    fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,20 +10,16 @@
   *         Otherwise, the first index where the value is located.
   * Description: Prints a value every time it is compared in the array.
   */
- 
+
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
-
-	if (array == NULL)
-		return (-1);
+	size_t i = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
-
 	return (-1);
 }
